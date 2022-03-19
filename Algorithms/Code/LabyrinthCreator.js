5f90d8324d8a3c09cf32b96ffe6896abfd156265
qayum001 <99470584+qayum001@users.noreply.github.com>
2022-03-19 15:10:54 +0700

    h

--- Algorithms/Code/LabyrinthCreator.js
+++ Algorithms/Code/LabyrinthCreator.js
@@ -1,41 +1,46 @@
 let canv = document.getElementById("matrix");
-
-canv.width = 1590;
-canv.height = 1000;
-
 let ctx = canv.getContext("2d");
+
+let rows = 70;
+let columns = 10;
+
+canv.width = rows * 10;
+canv.height = columns * 10;
+
+
 let pointToDrow = new Array();
-let rows = 159;
-let columns = 99;
+
 let lol = Labyrinth(matrixArray(rows, columns), rows, columns);
 window.requestAnimationFrame(printPoint);
+
 let indexToGetPoint = 0;
 
 function Labyrinth(lab, rows, columns){
-    let s = point(rand(1, rows / 2) * 2 - 1, rand(1, columns / 2) * 2 - 1);// s = start point
+    let s = point(rand(1, rows / 2) * 2 - 1, rand(1, columns / 2) * 2 - 1, 'trail');// s = start point
     pointToDrow.push(s);
     setPointType(lab, s.x, s.y, 0);
     
     let ways = new Array();
     if(isAvailable(s.x, s.y - 2, rows, columns)){
-        ways.push(new point(s.x, s.y - 2));
+        ways.push(new point(s.x, s.y - 2, 'inter'));
     }
     if(isAvailable(s.x, s.y + 2, rows, columns)) {
-        ways.push(new point(s.x, s.y + 2));
+        ways.push(new point(s.x, s.y + 2, 'inter'));
     }
     if(isAvailable(s.x - 2, s.y, rows, columns)){
-        ways.push(new point(s.x - 2, s.y));
+        ways.push(new point(s.x - 2, s.y, 'inter'));
     }
     if(isAvailable(s.x + 2, s.y, rows, columns)){
-        ways.push(new point(s.x + 2, s.y));
+        ways.push(new point(s.x + 2, s.y, 'inter'));
     }
     while(ways.length > 0){
         let getRandomPoint  = rand(0, ways.length);
         let curP = popIndex(ways, getRandomPoint);//current Point
+        curP.type = 'trail';
         pointToDrow.push(curP);
         if (lab[curP.x][curP.y] == 0){
             continue;
-        }            
+        }
         setPointType(lab, curP.x, curP.y, 0);
         let Direction = ['l', 't', 'r', 'b'];
         while(Direction.length > 0){
@@ -44,28 +49,28 @@ function Labyrinth(lab, rows, columns){
                 case 'l':
                     if(isAvailable(curP.x - 2, curP.y, rows, columns) && isClear(lab, curP.x - 2, curP.y)){
                         setPointType(lab, curP.x - 1, curP.y, 0);
-                        pointToDrow.push(new point(curP.x - 1, curP.y));
+                        pointToDrow.push(new point(curP.x - 1, curP.y, 'trail'));
                         Direction = [];
                     }
                 break;
                 case 't':
                     if(isAvailable(curP.x, curP.y - 2, rows, columns) && isClear(lab, curP.x, curP.y - 2)){
                         setPointType(lab, curP.x, curP.y - 1, 0)
-                        pointToDrow.push(new point(curP.x, curP.y - 1));
+                        pointToDrow.push(new point(curP.x, curP.y - 1, 'trail'));
                         Direction = [];
                     }
                 break;
                 case 'r':
                     if(isAvailable(curP.x + 2, curP.y, rows, columns) && isClear(lab, curP.x + 2, curP.y)){
                         setPointType(lab, curP.x + 1, curP.y, 0);
-                        pointToDrow.push(new point(curP.x + 1, curP.y));
+                        pointToDrow.push(new point(curP.x + 1, curP.y, 'trail'));
                         Direction = [];
                     }
                 break;
                 case 'b':
                     if(isAvailable(curP.x, curP.y + 2, rows, columns) && isClear(lab, curP.x, curP.y + 2)){
                         setPointType(lab, curP.x, curP.y + 1, 0);
-                        pointToDrow.push(new point(curP.x, curP.y + 1));
+                        pointToDrow.push(new point(curP.x, curP.y + 1, 'trail'));
                         Direction = [];
                     }
                 break;
@@ -73,31 +78,43 @@ function Labyrinth(lab, rows, columns){
             Direction.splice(dir, 1);
         }
         if(isAvailable(curP.x - 2, curP.y, rows, columns) && isWall(lab, curP.x - 2, curP.y)){
-            ways.push(new point(curP.x - 2, curP.y));
+            ways.push(new point(curP.x - 2, curP.y, 'inter'));
+            pointToDrow.push(new point(curP.x - 2, curP.y, 'inter'));
         }
         if(isAvailable(curP.x, curP.y - 2, rows, columns) && isWall(lab, curP.x, curP.y - 2)){
-            ways.push(new point(curP.x, curP.y - 2));
+            ways.push(new point(curP.x, curP.y - 2, 'inter'));
+            pointToDrow.push(new point(curP.x, curP.y - 2, 'inter'));
         }
         if(isAvailable(curP.x + 2, curP.y, rows, columns) && isWall(lab, curP.x + 2, curP.y)){
-            ways.push(new point(curP.x + 2, curP.y));
+            ways.push(new point(curP.x + 2, curP.y, 'inter'));
+            pointToDrow.push(new point(curP.x + 2, curP.y, 'inter'));
         }
         if(isAvailable(curP.x, curP.y + 2, rows, columns) && isWall(lab, curP.x, curP.y + 2)){
-            ways.push(new point(curP.x, curP.y + 2));
+            ways.push(new point(curP.x, curP.y + 2, 'inter'));
+            pointToDrow.push(new point(curP.x, curP.y + 2, 'inter'));
         }
     }
-    if (rows % 2 == 0)
-        lab = fixedRow(lab, rows, columns)
-    if (columns % 2 == 0)
-        lab = fixedColumn(lab, rows, columns)
+    // if (rows % 2 == 0)
+    //     lab = fixedRow(lab, rows, columns)
+    // if (columns % 2 == 0)
+    //     lab = fixedColumn(lab, rows, columns)
     return lab;
 }
+console.log(pointToDrow.length);
 function printPoint(){
+
+    let currentPoints = pointToDrow.splice(0, Math.ceil(pointToDrow.length * 0.025));
     console.log(pointToDrow.length);
-    let currentPoint = pointToDrow[indexToGetPoint];
-    indexToGetPoint++;
-    ctx.fillStyle = 'gray';
-    ctx.fillRect(currentPoint.x*10, currentPoint.y*10, 10, 10);
-    if(indexToGetPoint < pointToDrow.length){
+    for(let i = 0; i < currentPoints.length; i++){
+        if(currentPoints[i].type == 'inter'){
+            ctx.fillStyle = 'red';
+        }
+        if(currentPoints[i].type == 'trail'){
+            ctx.fillStyle = 'gray';
+        }
+        ctx.fillRect(currentPoints[i].x*10, currentPoints[i].y*10, 10, 10);   
+    }
+    if(pointToDrow.length > 0){
         window.requestAnimationFrame(printPoint);
     }
 }
@@ -111,10 +128,12 @@ function matrixArray(rows, columns) {//returns a matrix filled with Walls
     }
     return arr;
 }
-function point(x, y){
+function point(x, y, type){
+    //TODO: set defoult typr
     return {
         x: x = x,
         y: y = y,
+        type: type = type,
     };
 }
 function isAvailable(x, y, rows, columns) {//returns true if point is not out of matrix 
